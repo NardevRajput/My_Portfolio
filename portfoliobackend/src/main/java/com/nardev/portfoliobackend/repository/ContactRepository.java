343909c8c280eba90688cb01f4b75d92979ff870
Nardev Rajput <nardevrajput001@gmail.com>
2026-05-25 17:32:51 +0530

    Delete portfoliobackend directory

--- portfoliobackend/src/main/java/com/nardev/portfoliobackend/repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nardev.portfoliobackend.repository;
-
-import com.nardev.portfoliobackend.model.Contact;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactRepository
-                extends JpaRepository<Contact, Integer> {
-}
