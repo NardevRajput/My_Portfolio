742af335ca765067a296adcfdee837ae5b124b1c
Nardev Rajput <nardevrajput001@gmail.com>
2026-06-17 18:25:15 +0530

    Disable excel and email test

--- portfoliobackend/src/main/java/com/nardev/portfoliobackend/controller/ContactController.java
+++ portfoliobackend/src/main/java/com/nardev/portfoliobackend/controller/ContactController.java
@@ -1,74 +1,74 @@
-package com.nardev.portfoliobackend.controller;
+// package com.nardev.portfoliobackend.controller;
 
-import com.nardev.portfoliobackend.dto.ContactRequestDTO;
+// import com.nardev.portfoliobackend.dto.ContactRequestDTO;
 
-import jakarta.validation.Valid;
+// import jakarta.validation.Valid;
 
-import com.nardev.portfoliobackend.model.Contact;
+// import com.nardev.portfoliobackend.model.Contact;
 
-import com.nardev.portfoliobackend.repository.ContactRepository;
+// import com.nardev.portfoliobackend.repository.ContactRepository;
 
-import com.nardev.portfoliobackend.response.ApiResponse;
+// import com.nardev.portfoliobackend.response.ApiResponse;
 
-import com.nardev.portfoliobackend.service.EmailService;
-import com.nardev.portfoliobackend.service.ExcelService;
+// import com.nardev.portfoliobackend.service.EmailService;
+// import com.nardev.portfoliobackend.service.ExcelService;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.web.bind.annotation.*;
+// import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+// import java.util.List;
 
-@RestController
-@RequestMapping("/api/contact")
-// @CrossOrigin("*")
-public class ContactController {
+// @RestController
+// @RequestMapping("/api/contact")
+// // @CrossOrigin("*")
+// public class ContactController {
 
-    @Autowired
-    private ContactRepository contactRepository;
+// @Autowired
+// private ContactRepository contactRepository;
 
-    @Autowired
-    private ExcelService excelService;
+// @Autowired
+// private ExcelService excelService;
 
-    @Autowired
-    private EmailService emailService;
+// @Autowired
+// private EmailService emailService;
 
-    @PostMapping
-    public ApiResponse<Contact> saveContact(
-            @Valid @RequestBody ContactRequestDTO dto) {
+// @PostMapping
+// public ApiResponse<Contact> saveContact(
+// @Valid @RequestBody ContactRequestDTO dto) {
 
-        Contact contact = new Contact();
+// Contact contact = new Contact();
 
-        contact.setName(dto.getName());
-        contact.setEmail(dto.getEmail());
-        contact.setMessage(dto.getMessage());
+// contact.setName(dto.getName());
+// contact.setEmail(dto.getEmail());
+// contact.setMessage(dto.getMessage());
 
-        Contact savedContact = contactRepository.save(contact);
+// Contact savedContact = contactRepository.save(contact);
 
-        excelService.saveToExcel(contact);
-        emailService.sendContactEmail(contact);
+// excelService.saveToExcel(contact);
+// emailService.sendContactEmail(contact);
 
-        return new ApiResponse<>(
-                true,
-                "Contact saved successfully",
-                savedContact);
-    }
+// return new ApiResponse<>(
+// true,
+// "Contact saved successfully",
+// savedContact);
+// }
 
-    @GetMapping("/admin/contacts")
-    public List<Contact> getAllContacts() {
+// @GetMapping("/admin/contacts")
+// public List<Contact> getAllContacts() {
 
-        return contactRepository.findAll();
-    }
+// return contactRepository.findAll();
+// }
 
-    @DeleteMapping("/admin/{id}")
-    public ApiResponse<String> deleteContact(
-            @PathVariable("id") Integer id) {
+// @DeleteMapping("/admin/{id}")
+// public ApiResponse<String> deleteContact(
+// @PathVariable("id") Integer id) {
 
-        contactRepository.deleteById(id);
+// contactRepository.deleteById(id);
 
-        return new ApiResponse<>(
-                true,
-                "Contact deleted successfully",
-                null);
-    }
-}
+// return new ApiResponse<>(
+// true,
+// "Contact deleted successfully",
+// null);
+// }
+// }
