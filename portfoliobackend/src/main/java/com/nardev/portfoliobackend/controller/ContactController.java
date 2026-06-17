1a1cbf22545049aeed7080ee070315a4a679bbb7
Nardev Rajput <nardevrajput001@gmail.com>
2026-06-17 20:42:30 +0530

    Configure backend for deployment

--- portfoliobackend/src/main/java/com/nardev/portfoliobackend/controller/ContactController.java
+++ portfoliobackend/src/main/java/com/nardev/portfoliobackend/controller/ContactController.java
@@ -1,21 +1,74 @@
-@PostMapping
-public ApiResponse<Contact> saveContact(
-        @Valid @RequestBody ContactRequestDTO dto) {
+package com.nardev.portfoliobackend.controller;
 
-    Contact contact = new Contact();
+import com.nardev.portfoliobackend.dto.ContactRequestDTO;
 
-    contact.setName(dto.getName());
-    contact.setEmail(dto.getEmail());
-    contact.setMessage(dto.getMessage());
+import jakarta.validation.Valid;
 
-    Contact savedContact = contactRepository.save(contact);
+import com.nardev.portfoliobackend.model.Contact;
 
-    // TEMPORARY DISABLE
-    // excelService.saveToExcel(contact);
-    // emailService.sendContactEmail(contact);
+import com.nardev.portfoliobackend.repository.ContactRepository;
 
-    return new ApiResponse<>(
-            true,
-            "Contact saved successfully",
-            savedContact);
+import com.nardev.portfoliobackend.response.ApiResponse;
+
+import com.nardev.portfoliobackend.service.EmailService;
+import com.nardev.portfoliobackend.service.ExcelService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/contact")
+// @CrossOrigin("*")
+public class ContactController {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @Autowired
+    private ExcelService excelService;
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping
+    public ApiResponse<Contact> saveContact(
+            @Valid @RequestBody ContactRequestDTO dto) {
+
+        Contact contact = new Contact();
+
+        contact.setName(dto.getName());
+        contact.setEmail(dto.getEmail());
+        contact.setMessage(dto.getMessage());
+
+        Contact savedContact = contactRepository.save(contact);
+
+        excelService.saveToExcel(contact);
+        emailService.sendContactEmail(contact);
+
+        return new ApiResponse<>(
+                true,
+                "Contact saved successfully",
+                savedContact);
+    }
+
+    @GetMapping("/admin/contacts")
+    public List<Contact> getAllContacts() {
+
+        return contactRepository.findAll();
+    }
+
+    @DeleteMapping("/admin/{id}")
+    public ApiResponse<String> deleteContact(
+            @PathVariable("id") Integer id) {
+
+        contactRepository.deleteById(id);
+
+        return new ApiResponse<>(
+                true,
+                "Contact deleted successfully",
+                null);
+    }
 }
